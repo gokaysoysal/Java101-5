4ef28b39ecbb61a2de0d03585ec75f24f91e9f42
Gökay Soysal <agokaysoysal@gmail.com>
2022-07-03 23:30:15 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Car audi = new Car();
         audi.model = "Audi";
-        audi.speed = 10;
+        audi.speed = 20;
         audi.increaseSpeed(20);
         audi.printSpeed();
 
